f8a4b8ca927cb7b1d33481e7fc3f2a4aedf0e6d9
BB <biring@gmail.com>
2024-11-01 10:50:38 -0400

    Changed pom.xml file path to relative path

--- src/script/run_tests.bat
+++ src/script/run_tests.bat
@@ -1,8 +1,18 @@
 @echo off
+
+REM ### TO RUN ALL UNITS ###
+REM 1. Ensure Apache Maven is installed on the computer.
+REM 2. Ensure Windows environment variable path is set to Apache Maven bin folder.
+REM 3. Open terminal in IntelliJ or Windows command prompt
+REM 4. [Optional] Run command "mvn -version" to verify Apache Maven is set up correctly
+REM 5. Navigate to script folder in the project. Example = cd C:\Users\birin\Desktop\PersonalFinanceTracker\src\script
+REM 6. Run the command = .\run_tests.bat
+
+REM Limit variables created in this script to a local scope
 setlocal
 
-rem Change to the directory of your project root where pom.xml is located
-cd /d "C:\Users\birin\Desktop\PersonalFinanceTracker"
+REM Change to the directory of your project root where pom.xml is located
+cd ..\..
 
 rem Run Maven tests
 call mvn test
