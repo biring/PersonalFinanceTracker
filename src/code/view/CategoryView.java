9bcb9b3a008f3fe478648c02cd556704ee0dfcbf
BB <biring@gmail.com>
2024-11-13 20:14:52 -0500

    Initial relase of category view class and unittest

---
+++ src/code/view/CategoryView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Scanner;
+
+public class CategoryView extends BaseClass {
+
+    public CategoryView(Scanner scanner) {
+        super(scanner);
+    }
+
+    // You may add any view-specific behavior here if necessary
+
+}
