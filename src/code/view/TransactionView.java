c0623b5fe015192b64f1f8d3cecac30ab7941062
BB <biring@gmail.com>
2024-11-13 20:18:06 -0500

    Initial relase of transaction view class and unittest

---
+++ src/code/view/TransactionView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Scanner;
+
+public class TransactionView extends BaseClass {
+
+    public TransactionView(Scanner scanner) {
+        super(scanner);
+    }
+
+    // You can add any view-specific behavior here if necessary
+
+}
