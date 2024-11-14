fb44506f180e542f2a4e1550dd5283915cf4efcd
BB <biring@gmail.com>
2024-11-13 20:12:09 -0500

    Initial relase of account view class and unittest

---
+++ src/code/view/AccountView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Scanner;
+
+public class AccountView extends BaseClass {
+
+    public AccountView(Scanner scanner) {
+        super(scanner);
+    }
+
+    // You may add any view-specific behavior here if necessary
+}
+
