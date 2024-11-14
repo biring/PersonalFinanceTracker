e88438bfdb1829e2adf1a76793c849fff8a37e26
BB <biring@gmail.com>
2024-11-13 20:30:53 -0500

    Updated to use base class and add controller class calls

--- src/code/controller/MainController.java
+++ src/code/controller/MainController.java
@@ -4,26 +4,62 @@ import view.MainView;
 
 import java.util.Scanner;
 
-public class MainController {
-
-    // Instance variable to store the shared Scanner object
-    private final Scanner scanner;
-    private final MainView mainView;
+public class MainController extends BaseClass<MainView> {
     private final String[] mainMenuOption = {"Account", "Category", "Budget", "Link", "Transaction", "Report", "Exit"};
+    private final int EXIT_OPTION = mainMenuOption.length;
+
+    private AccountController accountController;
+    private CategoryController categoryController;
+    private BudgetController budgetController;
+    private LinkController linkController;
+    private TransactionController transactionController;
+    private ReportController reportController;
 
     // Constructor to initialize the shared Scanner object
     public MainController(Scanner scanner) {
-        this.scanner = scanner;
-        this.mainView = new MainView(scanner);
+        super(scanner, new MainView(scanner));
+    }
+
+    // Method to initialize the application
+    public void start(Scanner scanner) {
+        this.accountController = new AccountController(scanner);
+        this.categoryController = new CategoryController(scanner);
+        this.budgetController = new BudgetController(scanner);
+        this.reportController = new ReportController(scanner);
+        this.transactionController = new TransactionController(scanner);
+        this.linkController = new LinkController(scanner);
     }
 
     // Method to start the application flow
-    public void start() {
-        int input;
-        mainView.displayMenu(mainMenuOption);
-        input = mainView.promptForMenuSelection("Enter your choice: ");
-        System.out.println("User input: " + input);
+    public void run() {
+        int selection = 0;
+        do {
+            view.displayMenu(mainMenuOption);
+            selection = view.promptForMenuSelection("Enter your choice: ");
+            switch (selection) {
+                case 1:
+                    accountController.run();
+                    break;
+                case 2:
+                    categoryController.run();
+                    break;
+                case 3:
+                    budgetController.run();
+                    break;
+                case 4:
+                    linkController.run();
+                    break;
+                case 5:
+                    transactionController.run();
+                    break;
+                case 6:
+                    reportController.run();
+                    break;
+                case 7:
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value for main menu selection: " + selection);
+            }
+        } while (selection != EXIT_OPTION);
     }
-
-
 }
