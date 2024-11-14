36aafb04f7cfc26e505ef84f1218eae435bc13f3
BB <biring@gmail.com>
2024-11-13 20:13:47 -0500

    Initial relase of budget view class and unittest

---
+++ src/code/view/BudgetView.java
@@ -0,0 +1,11 @@
+package view;
+
+import java.util.Scanner;
+
+public class BudgetView extends BaseClass {
+    public BudgetView(Scanner scanner) {
+        super(scanner);
+    }
+
+    // You may add any view-specific behavior here if necessary
+}
