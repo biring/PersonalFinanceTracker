8efb6abbb3db7df8043c29c004597bb0da6ca7c3
BB <biring@gmail.com>
2024-11-13 20:16:00 -0500

    Initial relase of link view class and unittest

---
+++ src/code/view/LinkView.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.util.Scanner;
+
+public class LinkView extends BaseClass {
+
+    public LinkView(Scanner scanner) {
+        super(scanner);
+    }
+
+    // You may add any view-specific behavior here if necessary
+
+}
