75d7b3a7da5032b1b006380f0538d91d867b03d5
BB <biring@gmail.com>
2024-11-14 21:29:53 -0500

    Created methods to support account creation

--- src/code/view/AccountView.java
+++ src/code/view/AccountView.java
@@ -2,12 +2,37 @@ package view;
 
 import java.util.Scanner;
 
+import static view.messages.AccountMessages.*;
+
 public class AccountView extends BaseClass {
 
     public AccountView(Scanner scanner) {
         super(scanner);
     }
 
-    // You may add any view-specific behavior here if necessary
+    public String promptForAccountName() {
+        return promptForStringInput(PROMPT_ACCOUNT_NAME);
+    }
+
+    public String promptForAccountTypeSelection(String[] accountTypes) {
+        displayMenu(accountTypes);
+        int selection = promptForMenuSelection(PROMPT_ACCOUNT_TYPE);
+        return accountTypes[selection - 1];
+    }
+
+    public void showAccountValidResult(boolean success) {
+        if (success) {
+            printMessageLine(INFO_ACCOUNT_NAME_VALID);
+        } else {
+            printMessageLine(INFO_ACCOUNT_NAME_INVALID);
+        }
+    }
+    public void showAccountCreationResult(boolean success) {
+        if (success) {
+            printMessageLine(INFO_ACCOUNT_CREATED_SUCCESS);
+        } else {
+            printMessageLine(INFO_ACCOUNT_CREATION_FAILED);
+        }
+    }
 }
 
