ec306f4011dc8d2f8130c956cc4bc6b37b0fd67f
BB <biring@gmail.com>
2024-11-15 22:38:33 -0500

    Added interface to get integer input

--- src/code/view/Interface.java
+++ src/code/view/Interface.java
@@ -9,4 +9,7 @@ public interface Interface {
 
     // Method to prompt the user for a string input
     String promptForStringInput(String message);
+
+    // Method to prompt the user for an integer input
+    int promptForIntInput(String message);
 }
