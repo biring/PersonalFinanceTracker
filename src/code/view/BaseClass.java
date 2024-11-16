f67e9f69ce0db1dc988454e253633a9c475e40b3
BB <biring@gmail.com>
2024-11-16 10:51:08 -0500

    Removed string menu selection methods

--- src/code/view/BaseClass.java
+++ src/code/view/BaseClass.java
@@ -8,31 +8,9 @@ import utility.Console;
 
 public class BaseClass implements Interface {
     protected Scanner scanner;
-    protected int menuOptionMax;
 
     public BaseClass(Scanner scanner) {
         this.scanner = scanner;
-    }
-
-    @Override
-    public final void displayMenu(String[] menuOptions) {
-        this.menuOptionMax = menuOptions.length;
-        String formattedMenu = Console.formatList(menuOptions);
-        printMessageLine(formattedMenu);
-    }
-
-    @Override
-    public final int promptForMenuSelection(String message) {
-        while (true) {
-            printMessage(message);
-            String input = Console.getInput(scanner);
-            int selection = Console.stringToInt(input);
-            if (Console.isValidSelection(selection, menuOptionMax)) {
-                return selection;
-            } else {
-                printMessageLine("Invalid selection. Please enter a number between 1 and " + menuOptionMax);
-            }
-        }
     }
 
     @Override
