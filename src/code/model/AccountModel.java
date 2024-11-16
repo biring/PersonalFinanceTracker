f665674f251f197126a8fdd3e4b67055947e8b58
BB <biring@gmail.com>
2024-11-16 18:56:43 -0500

    Refactored to use name attribute and its getters and setters in the base class

--- src/code/model/AccountModel.java
+++ src/code/model/AccountModel.java
@@ -1,21 +1,12 @@
 package model;
 
 public class AccountModel extends BaseModel {
-    private String name;
+
     private final AccountType accountType;
 
     public AccountModel(int id, String name, AccountType type) {
-        super(id);
-        this.name = name;
+        super(id, name);
         this.accountType = type;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public AccountType getAccountType() {
