5e59c5f8a32e2e49280866436e376846a1152140
BB <biring@gmail.com>
2024-11-15 22:05:18 -0500

    Created account data access object for account model

---
+++ src/code/dao/AccountDao.java
@@ -0,0 +1,33 @@
+package dao;
+
+import model.AccountModel;
+import model.BaseModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDao {
+    private final List<AccountModel> accounts = new ArrayList<>();
+
+    // get the next available ID
+    public int getNextID() {
+        int nextID = 0;
+        for (AccountModel account : accounts) {
+            if (nextID <= account.getID()) {
+                nextID = account.getID();
+            }
+        }
+        return nextID + 1;
+    }
+
+    // Create a new account
+    public void addAccount(int id, String name) {
+        AccountModel account = new AccountModel(id, name);
+        accounts.add(account);
+    }
+
+    // Retrieve all accounts
+    public List<AccountModel> getAllAccounts() {
+        return accounts;
+    }
+}
