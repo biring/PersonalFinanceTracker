c2f167adfb8ff495b142b3c2d2d9b00d84ef2b41
BB <biring@gmail.com>
2024-11-16 10:50:13 -0500

    Removed string menu selection interface

--- src/code/view/Interface.java
+++ src/code/view/Interface.java
@@ -1,11 +1,6 @@
 package view;
 
 public interface Interface {
-    // Method to display the main menu options
-    void displayMenu(String[] menuOptions);
-
-    // Method to prompt the user for a selection
-    int promptForMenuSelection(String message);
 
     // Method to prompt the user for a string input
     String promptForStringInput(String message);
