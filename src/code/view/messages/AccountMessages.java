53bfeb33704ed2d1d0378586d8ef479ac9310f27
BB <biring@gmail.com>
2024-11-16 00:42:54 -0500

    Refactored account creation status message

--- src/code/view/messages/AccountMessages.java
+++ src/code/view/messages/AccountMessages.java
@@ -20,12 +20,12 @@ public class AccountMessages {
     // INFO_* for general information messages
     public static final String INFO_ACCOUNT_NAME_INVALID = "Account name is invalid.";
     public static final String INFO_ACCOUNT_NAME_VALID = "Account name is valid.";
-    public static final String INFO_ACCOUNT_CREATED_SUCCESS = "Account created successfully.";
-    public static final String INFO_ACCOUNT_CREATION_FAILED = "Account creation failed.";
+    public static final String INFO_ACCOUNT_CREATION_SUCCESS = "Account created successfully.";
     public static final String INFO_ACCOUNT_MODIFIED_SUCCESS = "Account modified successfully.";
     public static final String INFO_ACCOUNT_DELETED_SUCCESS = "Account deleted successfully.";
 
     // WARNING_* for warning messages
+    public static final String WARNING_ACCOUNT_CREATION_FAILED = "Account creation failed.";
     public static final String WARNING_ACCOUNT_MODIFICATION_FAILED = "Account modification failed.";
     public static final String WARNING_ACCOUNT_DELETION_FAILED = "Account deletion failed.";
 
