5d9a6bd108bdde3f3992665e279522ad98bcb084
BB <biring@gmail.com>
2024-11-16 14:02:39 -0500

    Updated to remove unused interface

--- src/code/view/Interface.java
+++ src/code/view/Interface.java
@@ -4,13 +4,4 @@ public interface Interface {
 
     // Method to prompt the user for a string input
     String promptForStringInput(String message);
-
-    // Method to prompt the user for an integer input
-    int promptForIntInput(String message);
-
-    // Method to print menu title
-    void printMenuTitle(String title);
-
-    // Method to print table title
-    void printTableTitle(String title);
 }
