268cbfb3956a20811c808b568df5ff8710a1304f
BB <biring@gmail.com>
2024-11-16 18:51:44 -0500

    Added methods to support category management

--- src/code/view/CategoryView.java
+++ src/code/view/CategoryView.java
@@ -1,11 +1,46 @@
 package view;
 
+import controller.MenuOption;
+
+import java.util.List;
+
+import static view.messages.CategoryMessages.*;
+
 public class CategoryView extends BaseClass {
 
     public CategoryView() {
         super();
     }
 
-    // You may add any view-specific behavior here if necessary
+    public <T extends Enum<T> & MenuOption> T promptForEnumMenuSelection(Class<T> enumType) {
+        return super.getEnumMenuSelection(enumType, TITLE_MENU, PROMPT_FOR_MENU_SELECTION);
+    }
 
+    public String promptForCategoryName() {
+        return super.promptForStringInput(PROMPT_FOR_NAME);
+    }
+
+    public int promptForCategorySelection(List<String> categories) {
+        return super.getTableSelection(categories, TITLE_TABLE, PROMPT_FOR_SELECTION_BY_NO);
+    }
+
+    public void showCategories(List<String> categories) {
+        super.displayTableData(categories, TITLE_TABLE);
+    }
+
+    public void showCategoryNameValidResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_NAME_VALID, WARNING_NAME_INVALID);
+    }
+
+    public void showCategoryCreationResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_CREATION_SUCCESS, WARNING_CREATION_FAILED);
+    }
+
+    public void showCategoryModificationResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_MODIFICATION_SUCCESS, WARNING_MODIFICATION_FAILED);
+    }
+
+    public void showCategoryDeletionResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_DELETION_SUCCESS, WARNING_DELETION_FAILED);
+    }
 }
