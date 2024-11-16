f2ac10b319c5c355bcc9df79fd70aaf64a15c507
BB <biring@gmail.com>
2024-11-16 18:55:49 -0500

    Added name attribure and its getters and setters to base class

--- src/code/model/BaseModel.java
+++ src/code/model/BaseModel.java
@@ -2,12 +2,22 @@ package model;
 
 public class BaseModel {
     protected int id;
+    protected String name;
 
-    public BaseModel(int id) {
+    public BaseModel(int id, String name) {
         this.id = id;
+        this.name = name;
     }
 
     public final int getID() {
         return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
