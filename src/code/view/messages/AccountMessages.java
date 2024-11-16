cc721a608f0b689940864995d242949cc5a688c7
BB <biring@gmail.com>
2024-11-16 17:14:46 -0500

    Added strings to support accout type creation

--- src/code/view/messages/AccountMessages.java
+++ src/code/view/messages/AccountMessages.java
@@ -16,15 +16,18 @@ public class AccountMessages {
     // TITLE_* for titles of menu, tables, etc.
     public static final String TITLE_ACCOUNT_MENU = "Account Menu";
     public static final String TITLE_ACCOUNT_TABLE = "Accounts List";
+    public static final String TITLE_ACCOUNT_TYPE = "Account Types";
 
     // INFO_* for general information messages
     public static final String INFO_ACCOUNT_NAME_INVALID = "Account name is invalid.";
     public static final String INFO_ACCOUNT_NAME_VALID = "Account name is valid.";
+    public static final String INFO_ACCOUNT_TYPE_VALID = "Account type is valid.";
     public static final String INFO_ACCOUNT_CREATION_SUCCESS = "Account created successfully.";
     public static final String INFO_ACCOUNT_MODIFIED_SUCCESS = "Account modified successfully.";
     public static final String INFO_ACCOUNT_DELETED_SUCCESS = "Account deleted successfully.";
 
     // WARNING_* for warning messages
+    public static final String WARNING_ACCOUNT_TYPE_INVALID = "Account type is invalid.";
     public static final String WARNING_ACCOUNT_CREATION_FAILED = "Account creation failed.";
     public static final String WARNING_ACCOUNT_MODIFICATION_FAILED = "Account modification failed.";
     public static final String WARNING_ACCOUNT_DELETION_FAILED = "Account deletion failed.";
