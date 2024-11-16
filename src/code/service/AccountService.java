cbac929846568ee40e27d562e190a07a03791651
BB <biring@gmail.com>
2024-11-16 10:26:52 -0500

    Updated account creation method to check new accout name is not in use and meets length requirements

--- src/code/service/AccountService.java
+++ src/code/service/AccountService.java
@@ -6,13 +6,20 @@ import java.util.List;
 
 public class AccountService {
 
+    private static final int MIN_ACCOUNT_NAME_LENGTH = 3;
+    private static final int MAX_ACCOUNT_NAME_LENGTH = 20;
+
     private final AccountDao accountDao = new AccountDao();
 
     public AccountService() {
     }
 
     public boolean isAccountNameValid(String accountName) {
-        return accountName != null && !accountName.isEmpty();
+        return (accountName != null)
+                && (!accountName.isEmpty())
+                && (!accountDao.isAccountNameExists(accountName))
+                && (accountName.length() >= MIN_ACCOUNT_NAME_LENGTH)
+                && (accountName.length() <= MAX_ACCOUNT_NAME_LENGTH);
     }
 
     public boolean createAccount(String name) {
