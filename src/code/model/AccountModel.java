bbb26f2c77415641516b71601bb734fbd2266aba
BB <biring@gmail.com>
2024-11-16 17:19:08 -0500

    Added methods to link account type to account model

--- src/code/model/AccountModel.java
+++ src/code/model/AccountModel.java
@@ -2,10 +2,12 @@ package model;
 
 public class AccountModel extends BaseModel {
     private String name;
+    private final AccountType accountType;
 
-    public AccountModel(int id, String name) {
+    public AccountModel(int id, String name, AccountType type) {
         super(id);
         this.name = name;
+        this.accountType = type;
     }
 
     public String getName() {
@@ -16,8 +18,12 @@ public class AccountModel extends BaseModel {
         this.name = name;
     }
 
+    public AccountType getAccountType() {
+        return this.accountType;
+    }
+
     @Override
     public String toString() {
-        return "Account: Id=" + super.id + ", Name=" + this.name;
+        return "Account: Id=" + super.id + ", Name=" + this.name + ", Type=" + this.accountType;
     }
 }
