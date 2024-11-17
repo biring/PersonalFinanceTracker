47281f86b3efa5ec1f8c9ff07c2be75d345f7ed7
BB <biring@gmail.com>
2024-11-16 23:19:30 -0500

    Added methods to support UI interaction for budget management

--- src/code/view/BudgetView.java
+++ src/code/view/BudgetView.java
@@ -1,9 +1,43 @@
 package view;
 
+import controller.MenuOption;
+
+import java.util.List;
+
+import static view.messages.BudgetMessages.*;
+
 public class BudgetView extends BaseClass {
+
     public BudgetView() {
         super();
     }
 
-    // You may add any view-specific behavior here if necessary
+    public <T extends Enum<T> & MenuOption> T promptForEnumMenuSelection(Class<T> enumType) {
+        return super.getEnumMenuSelection(enumType, TITLE_MENU, PROMPT_FOR_MENU_SELECTION);
+    }
+
+    public int promptForCategorySelection(List<String> categories) {
+        return super.getTableSelection(categories, TITLE_NO_BUDGET_TABLE, PROMPT_FOR_SELECTION_BY_NO);
+    }
+
+    public int promptForCategoryBudget() {
+        return super.promptForIntInput(PROMPT_FOR_CATEGORY_BUDGET);
+    }
+
+    public void showBudgets(List<String> budgets) {
+        super.displayTableData(budgets, TITLE_BUDGET_TABLE);
+    }
+
+    public void showBudgetCreationResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_CREATION_SUCCESS, WARNING_CREATION_FAILED );
+    }
+
+    public void showBudgetModificationResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_MODIFICATION_SUCCESS, WARNING_MODIFICATION_FAILED);
+    }
+
+    public void showBudgetDeletionResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_DELETION_SUCCESS, WARNING_DELETION_FAILED);
+    }
+
 }
