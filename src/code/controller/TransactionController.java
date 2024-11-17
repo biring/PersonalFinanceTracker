fc22852a9485e4959ec1378c123298c54090647f
BB <biring@gmail.com>
2024-11-17 17:16:03 -0500

    Added methods to implement transaction menu

--- src/code/controller/TransactionController.java
+++ src/code/controller/TransactionController.java
@@ -9,6 +9,72 @@ public class TransactionController extends BaseClass<TransactionView> {
     }
 
     public void run() {
+        TransactionController.enumMenuOptions selectedOption;
+        do {
+            selectedOption = view.promptForEnumMenuSelection(enumMenuOptions.class);
+            switch (selectedOption) {
+                case CREATE:
+                    createTransaction();
+                    break;
+                case MODIFY:
+                    modifyTransaction();
+                    break;
+                case VIEW:
+                    viewTransaction();
+                    break;
+                case DELETE:
+                    deleteTransaction();
+                    break;
+                case IMPORT:
+                    importTransaction();
+                    break;
+                case EXIT:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    throw new IllegalStateException("Invalid value for selection.");
+            }
+        }
+        while (selectedOption != enumMenuOptions.EXIT);
+    }
 
+    private void createTransaction() {
+        view.showMenuOptionNotSupported();
+    }
+
+    private void modifyTransaction() {
+        view.showMenuOptionNotSupported();
+    }
+
+    private void viewTransaction() {
+        view.showMenuOptionNotSupported();
+    }
+
+    private void deleteTransaction() {
+        view.showMenuOptionNotSupported();
+    }
+
+    private void importTransaction() {
+        view.showMenuOptionNotSupported();
+    }
+
+    private enum enumMenuOptions implements MenuOption {
+        CREATE("Create Transaction"),
+        MODIFY("Modify Transaction"),
+        VIEW("View Transaction"),
+        DELETE("Delete Transaction"),
+        IMPORT("Import Transaction"),
+        EXIT("Exit");
+
+        private final String text;
+
+        enumMenuOptions(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public String getText() {
+            return text;
+        }
     }
 }
