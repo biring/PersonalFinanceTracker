66ed0b483d5e8c956ef27fcc70367ba598018989
BB <biring@gmail.com>
2024-11-17 13:50:10 -0500

    Added new UI strings for budget selection messages

--- src/code/view/messages/BudgetMessages.java
+++ src/code/view/messages/BudgetMessages.java
@@ -11,10 +11,12 @@ public class BudgetMessages {
     public static final String TITLE_NO_BUDGET_TABLE = "List of categories without budgets";
     public static final String TITLE_BUDGET_TABLE = "List of categories with budgets";
     // INFO_* for general information messages
+    public static final String INFO_CATEGORY_NOT_AVAILABLE = "No Category available.";
     public static final String INFO_CREATION_SUCCESS = "Budget creation successful.";
     public static final String INFO_MODIFICATION_SUCCESS = "Budget modification successful.";
     public static final String INFO_DELETION_SUCCESS = "Budget deletion successful.";
     // WARNING_* for warning messages
+    public static final String WARNING_INVALID_SELECTION = "Invalid selection.";
     public static final String WARNING_CREATION_FAILED = "Budget creation failed.";
     public static final String WARNING_MODIFICATION_FAILED = "Budget modification failed.";
     public static final String WARNING_DELETION_FAILED = "Budget deletion failed.";
