67cfdc2a19a3c824586228633b904c0232431496
BB <biring@gmail.com>
2024-11-17 17:15:25 -0500

    Added methods to support transaction menu

--- src/code/view/TransactionView.java
+++ src/code/view/TransactionView.java
@@ -1,11 +1,22 @@
 package view;
 
+import controller.MenuOption;
+import utility.Console;
+
+import static view.messages.TransactionMessages.*;
+
 public class TransactionView extends BaseClass {
 
     public TransactionView() {
         super();
     }
 
-    // You can add any view-specific behavior here if necessary
+    public <T extends Enum<T> & MenuOption> T promptForEnumMenuSelection(Class<T> enumType) {
+        return super.getEnumMenuSelection(enumType, TITLE_MENU, PROMPT_FOR_MENU_SELECTION);
+    }
+
+    public void showMenuOptionNotSupported() {
+        Console.printMessage(INFO_SELECTION_NOT_SUPPORTED);
+    }
 
 }
