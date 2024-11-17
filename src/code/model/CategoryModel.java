e1e516020e530b42e0981cb88e2af72ae4dee437
BB <biring@gmail.com>
2024-11-16 23:25:05 -0500

    Added budget attribute along with getter and setter

--- src/code/model/CategoryModel.java
+++ src/code/model/CategoryModel.java
@@ -2,7 +2,18 @@ package model;
 
 public class CategoryModel extends BaseModel {
 
+    private int categoryBudget;
+
     public CategoryModel(int id, String name) {
         super(id, name);
+        this.categoryBudget = -1; // Default value mean no budget set
+    }
+
+    public int getCategoryBudget() {
+        return this.categoryBudget;
+    }
+
+    public void setCategoryBudget(int categoryBudget) {
+        this.categoryBudget = categoryBudget;
     }
 }
