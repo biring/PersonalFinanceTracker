a198745531f3c102412d52f885fb796b9626ceac
BB <biring@gmail.com>
2024-11-17 22:34:38 -0500

    Added messages to support csv file read

--- src/code/view/messages/TransactionMessages.java
+++ src/code/view/messages/TransactionMessages.java
@@ -3,21 +3,18 @@ package view.messages;
 public class TransactionMessages {
 
     // PROMPT_* for input prompts or messages requesting user action
-    public static final String PROMPT_FOR_MENU_SELECTION = "Enter your choice by number: ";
+    public static final String PROMPT_FOR_MENU = "Enter menu choice by number: ";
+    public static final String PROMPT_FOR_FILE = "Enter csv file choice by number: ";
     // TITLE_* for titles of menu, tables, etc.
     public static final String TITLE_MENU = "Transaction Menu";
-    public static final String TITLE_TRANSACTION_TABLE = "List of transaction";
+    public static final String TITLE_FILE = "List of csv files";
     // INFO_* for general information messages
-    public static final String INFO_SELECTION_NOT_SUPPORTED = "Selection not supported.";
-    public static final String INFO_TRANSACTIONS_NOT_AVAILABLE = "No transaction available.";
-    public static final String INFO_CREATION_SUCCESS = "Transaction creation successful.";
-    public static final String INFO_MODIFICATION_SUCCESS = "Transaction modification successful.";
-    public static final String INFO_DELETION_SUCCESS = "Transaction deletion successful.";
+    public static final String INFO_MENU_SELECTION_NOT_SUPPORTED = "Selection not supported.";
+    public static final String INFO_NO_CSV_FILE_FOUND = "No csv data file found.";
+    public static final String INFO_CSV_FILE_READ_SUCCESS = "CSV file read successful.";
     // WARNING_* for warning messages
     public static final String WARNING_INVALID_SELECTION = "Invalid selection.";
-    public static final String WARNING_CREATION_FAILED = "Transaction creation failed.";
-    public static final String WARNING_MODIFICATION_FAILED = "Transaction modification failed.";
-    public static final String WARNING_DELETION_FAILED = "Transaction deletion failed.";
+    public static final String WARNING_CSV_FILE_READ_FAILED = "CSV file read failed.";
 
     // Prevent instantiation
     private TransactionMessages() {
