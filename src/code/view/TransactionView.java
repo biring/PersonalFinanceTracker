bba7f7695ae08a3d2e07a09ae55abcb10e2c8d0f
BB <biring@gmail.com>
2024-11-17 22:35:25 -0500

    Added methods to support csv file read

--- src/code/view/TransactionView.java
+++ src/code/view/TransactionView.java
@@ -3,6 +3,8 @@ package view;
 import controller.MenuOption;
 import utility.Console;
 
+import java.util.List;
+
 import static view.messages.TransactionMessages.*;
 
 public class TransactionView extends BaseClass {
@@ -12,11 +14,31 @@ public class TransactionView extends BaseClass {
     }
 
     public <T extends Enum<T> & MenuOption> T promptForEnumMenuSelection(Class<T> enumType) {
-        return super.getEnumMenuSelection(enumType, TITLE_MENU, PROMPT_FOR_MENU_SELECTION);
+        return super.getEnumMenuSelection(enumType, TITLE_MENU, PROMPT_FOR_MENU);
     }
 
     public void showMenuOptionNotSupported() {
-        Console.printMessage(INFO_SELECTION_NOT_SUPPORTED);
+        Console.printMessage(INFO_MENU_SELECTION_NOT_SUPPORTED);
+    }
+
+    public void showNoCSVFilesFound() {
+        Console.printMessage(INFO_NO_CSV_FILE_FOUND);
+    }
+
+    public int promptForCSVFileSelection(List<String> files) {
+        return super.getTableSelection(files, TITLE_FILE, PROMPT_FOR_FILE);
+    }
+
+    public void showInvalidSelection() {
+        Console.printMessage(WARNING_INVALID_SELECTION);
+    }
+
+    public int promptForCSVFileSelection() {
+        return super.promptForIntInput(PROMPT_FOR_FILE);
+    }
+
+    public void showCSVFileReadResult(boolean success) {
+        super.displaySuccessFailureMessage(success, INFO_CSV_FILE_READ_SUCCESS, WARNING_CSV_FILE_READ_FAILED);
     }
 
 }
