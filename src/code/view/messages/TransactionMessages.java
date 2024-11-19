5cea09e03ca570814566ca762e7171028163fc09
BB <biring@gmail.com>
2024-11-18 21:03:54 -0500

    Added message strings to support account selection

--- src/code/view/messages/TransactionMessages.java
+++ src/code/view/messages/TransactionMessages.java
@@ -4,12 +4,15 @@ public class TransactionMessages {
 
     // PROMPT_* for input prompts or messages requesting user action
     public static final String PROMPT_FOR_MENU = "Enter menu choice by number: ";
+    public static final String PROMPT_FOR_ACCOUNT = "Enter account choice by number: ";
     public static final String PROMPT_FOR_FILE = "Enter csv file choice by number: ";
     // TITLE_* for titles of menu, tables, etc.
     public static final String TITLE_MENU = "Transaction Menu";
+    public static final String TITLE_ACCOUNT = "List of available accounts";
     public static final String TITLE_FILE = "List of csv files";
     // INFO_* for general information messages
     public static final String INFO_MENU_SELECTION_NOT_SUPPORTED = "Selection not supported.";
+    public static final String INFO_NO_ACCOUNTS_FOUND = "No accounts found.";
     public static final String INFO_NO_CSV_FILE_FOUND = "No csv data file found.";
     public static final String INFO_CSV_FILE_READ_SUCCESS = "CSV file read successful.";
     // WARNING_* for warning messages
