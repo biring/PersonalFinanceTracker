6087f1a4560868645cc960a900de4cb807d09ded
BB <biring@gmail.com>
2024-11-20 21:47:50 -0500

    Added methods to serialize/deserialize data model to/from json for database write/read

--- src/code/dao/BaseDAO.java
+++ src/code/dao/BaseDAO.java
@@ -1,11 +1,23 @@
 package dao;
 
+import utility.Json;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.reflect.TypeToken;
 
 public abstract class BaseDAO<T> {
     protected final List<T> items = new ArrayList<>();
     protected int nextID = 0;
+
+    // Store the class type for serialization/deserialization
+    private final Class<T> type;
+
+    public BaseDAO(Class<T> type) {
+        this.type = type;
+    }
 
     // Get the next available ID
     public int getNextID() {
@@ -50,4 +62,27 @@ public abstract class BaseDAO<T> {
 
     // Abstract method to extract ID from an item
     protected abstract int extractID(T item);
+
+
+    // Serialize the items list to a file
+    public void serialize() throws IOException {
+        Json.writeToFile(type.getSimpleName(), items); // Use the class type for the file name
+    }
+
+    // Deserialize the items list from a file
+    public void deserialize() throws IOException {
+        Type typeOfT = TypeToken.getParameterized(List.class, this.type).getType();  // Use the correct type for List<TransactionModel>
+        List<T> deserializedItems = Json.readFromFile(this.getClass().getSimpleName(), typeOfT);
+
+        if (deserializedItems != null) {
+            items.clear();
+            items.addAll(deserializedItems);
+
+            // Update nextID based on deserialized items
+            this.nextID = deserializedItems.stream()
+                    .mapToInt(this::extractID)
+                    .max()
+                    .orElse(0) + 1;
+        }
+    }
 }
