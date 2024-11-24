4fbd3c7f60503fcb7be510e1d0115c1fdfff43e2
BB <biring@gmail.com>
2024-11-23 19:13:14 -0500

    Updated budget service class to use category service class instead of category data access object class

--- src/code/service/BudgetService.java
+++ src/code/service/BudgetService.java
@@ -6,49 +6,29 @@ import java.util.List;
 
 public class BudgetService {
 
-    private CategoryDAO categoryDAO = null;
+    private final CategoryService categoryService;
 
-    public static final int NO_BUDGET = CategoryDAO.NO_BUDGET;
-
-    public BudgetService(CategoryDAO categoryDAO) {
-        this.categoryDAO = categoryDAO;
+    public BudgetService(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     public List<String> showCategoriesWithoutBudgets() {
-        return categoryDAO.readAll().stream()
-                .filter(category -> category.getCategoryBudget() <= NO_BUDGET)
-                .map(category -> "[" + category.getID() + "] " + category.getName() + " ($" + category.getCategoryBudget() + ")")
-                .toList();
+        return categoryService.showCategoriesWithoutBudgets();
     }
 
     public List<String> showCategoriesWithBudgets() {
-        return categoryDAO.readAll().stream()
-                .filter(category -> category.getCategoryBudget() > NO_BUDGET)
-                .map(category -> "[" + category.getID() + "] " + category.getName() + " ($" + category.getCategoryBudget() + ")")
-                .toList();
+        return categoryService.showCategoriesWithBudgets();
     }
 
     public boolean isCategoryWithoutBudget(int id) {
-        try {
-            return (categoryDAO.read(id).getCategoryBudget() <= NO_BUDGET);
-        } catch (NullPointerException e) {
-            return false;
-        }
+        return categoryService.isCategoryWithoutBudget(id);
     }
 
     public boolean setBudget(int id, int budget) {
-        try {
-            return categoryDAO.updateBudget(id, budget);
-        } catch (NullPointerException e) {
-            return false;
-        }
+        return categoryService.setBudget(id, budget);
     }
 
     public boolean resetBudget(int id) {
-        try {
-            return categoryDAO.updateBudget(id, NO_BUDGET);
-        } catch (NullPointerException e) {
-            return false;
-        }
+        return categoryService.resetBudget(id);
     }
 }
