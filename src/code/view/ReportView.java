c3aeb289d44747ec05c3a2a7825d7e5021e2bb0a
BB <biring@gmail.com>
2024-11-24 22:18:01 -0500

    Added methods for report menu selection and display report

--- src/code/view/ReportView.java
+++ src/code/view/ReportView.java
@@ -1,10 +1,26 @@
 package view;
 
+import controller.MenuOption;
+
+import java.util.List;
+
+import static view.messages.ReportMessages.*;
+
 public class ReportView  extends BaseClass {
 
     public ReportView() {
         super();
     }
 
-    // You can add any view-specific behavior here if necessary
+    public <T extends Enum<T> & MenuOption> T promptForEnumMenuSelection(Class<T> enumType) {
+        return super.getEnumMenuSelection(enumType, TITLE_MENU, PROMPT_FOR_MENU);
+    }
+
+    public void showAccountReport(List<String> report) {
+        super.displayTableData(report, TITLE_ACCOUNT_REPORT);
+    }
+
+    public void showBudgetReport(List<String> report) {
+        super.displayTableData(report, TITLE_BUDGET_REPORT);
+    }
 }
