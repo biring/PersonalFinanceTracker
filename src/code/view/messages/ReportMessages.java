fccb5aa01bfdaee9c107e1d18f51efd7dbb2adf2
BB <biring@gmail.com>
2024-11-24 22:16:09 -0500

    Created message string to support report management

---
+++ src/code/view/messages/ReportMessages.java
@@ -0,0 +1,18 @@
+package view.messages;
+
+public class ReportMessages {
+
+    // PROMPT_* for input prompts or messages requesting user action
+    public static final String PROMPT_FOR_MENU = "Enter menu choice by number: ";
+    // TITLE_* for titles of menu, tables, etc.
+    public static final String TITLE_MENU = "Report Menu";
+    public static final String TITLE_ACCOUNT_REPORT = "Account Report";
+    public static final String TITLE_BUDGET_REPORT = "Budget Report";
+    // INFO_* for general information messages
+    // WARNING_* for warning messages
+    // Prevent instantiation
+    private ReportMessages() {
+        throw new UnsupportedOperationException("Cannot instantiate this class.");
+    }
+}
+
