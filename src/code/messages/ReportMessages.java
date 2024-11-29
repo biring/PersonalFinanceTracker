7209aa563b5da248410fc190f545c50986f169ec
BB <biring@gmail.com>
2024-11-29 14:27:14 -0500

    refactor: Added account report and budget report table strings

--- src/code/messages/ReportMessages.java
+++ src/code/messages/ReportMessages.java
@@ -2,6 +2,25 @@ package messages;
 
 public class ReportMessages {
 
+    // STRING CONSTANTS
+    public static final String BUDGET_REPORT_COLUMN_1 = "[Category]";
+    public static final String BUDGET_REPORT_COLUMN_2 = "[Budget]";
+    public static final String BUDGET_REPORT_COLUMN_3 = "[Actual]";
+    public static final String BUDGET_REPORT_COLUMN_4 = "[Status]";
+
+    public static final String ACCOUNT_REPORT_COLUMN_1 = "[Account]";
+    public static final String ACCOUNT_REPORT_COLUMN_2 = "[Type]";
+    public static final String ACCOUNT_REPORT_COLUMN_3 = "[Credits]";
+    public static final String ACCOUNT_REPORT_COLUMN_4 = "[Debits]";
+    public static final String ACCOUNT_REPORT_COLUMN_5 = "[Net]";
+
+    public static final String STRING_TOTAL = "Total";
+    public static final String STRING_DASH = "-";
+    public static final String STRING_ELLIPSIS = "...";
+
+
+
+
     // PROMPT_* for input prompts or messages requesting user action
     public static final String PROMPT_FOR_MENU = "Enter menu choice by number: ";
     // TITLE_* for titles of menu, tables, etc.
