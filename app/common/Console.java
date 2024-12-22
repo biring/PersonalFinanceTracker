8d531c71aa2146a2313a94a82e09af6264aea794
BB <biring@gmail.com>
2024-12-22 14:41:24 -0500

    refactor(console): improve utility class design
    Prevent instantiation with private constructor.
    
    Add Javadoc comments for all methods.
    
    Enhance input validation and messaging functionality.

--- app/common/Console.java
+++ app/common/Console.java
@@ -2,43 +2,67 @@ package common;
 
 import java.util.Scanner;
 
+/**
+ * The {@code Console} class contains methods for interacting with the console.
+ * It provides static methods to read and write to console.
+ * This class is designed as a utility class and cannot be instantiated.
+ */
 public class Console {
 
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+    /**
+     * Private constructor prevents instantiation of the Console class.
+     */
+    private Console() {
+        throw new UnsupportedOperationException("Cannot instantiate Console class.");
     }
 
-    public static void waitForEnter(Scanner scanner) {
-        scanner.nextLine();
-    }
-
+    /**
+     * Retrieves a non-empty string input from the user using the provided Scanner object.
+     *
+     * @param scanner the Scanner object to use for input
+     * @return a non-empty string input provided by the user
+     */
     public static String getStringInput(Scanner scanner) {
         while (true) {
             String input = scanner.nextLine();
             if (input.isBlank()) {
-                Console.printMessage("Invalid input. Please enter a string.");
+                Console.printPrompt("Invalid input. Please enter a string: ");
             } else {
                 return input;
             }
         }
     }
 
-
+    /**
+     * Retrieves an integer input from the user using the provided Scanner object.
+     *
+     * @param scanner the Scanner object to use for input
+     * @return the integer input provided by the user
+     */
     public static int getIntInput(Scanner scanner) {
         while (true) {
             try {
                 return Integer.parseInt(getStringInput(scanner));
             } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a number.");
+                Console.printPrompt("Invalid input. Please enter a number: ");
             }
         }
     }
 
+    /**
+     * Prints the given message as a prompt without a new line.
+     *
+     * @param message the message to be displayed as a prompt
+     */
     public static void printPrompt(String message) {
         System.out.print(message);
     }
 
+    /**
+     * Prints the given message to the console followed by a new line.
+     *
+     * @param message the message to be printed to the console
+     */
     public static void printMessage(String message) {
         System.out.println(message);
     }
