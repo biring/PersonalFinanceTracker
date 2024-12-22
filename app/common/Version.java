14cbfd5e0f75646b70866676d5aae832e453c018
BB <biring@gmail.com>
2024-12-22 18:00:41 -0500

    feat(model): enhance LinkModel functionality and docs
    Add Javadoc comments to improve code documentation for LinkModel.
    
    Introduce toStringFormat for standardized string representation.
    
    Refactor toString method to use String.format for better readability.

--- app/common/Version.java
+++ app/common/Version.java
@@ -21,7 +21,7 @@ public class Version {
     private static final int patch = 0;
 
     // Build version number
-    private static final int build = 48;
+    private static final int build = 49;
 
     // Format string for version
     private static final String versionFormat = "Version %d.%d.%d";
--- app/link/model/LinkModel.java
+++ app/link/model/LinkModel.java
@@ -2,24 +2,61 @@ package link.model;
 
 import base.BaseModel;
 
+/**
+ * This is a model class that represents a link sting (name) to transaction and category.
+ * <p>
+ * This class extends {@link base.BaseModel} and adds functionality
+ * for holding link of name used to link category budget amount, providing access to it, and
+ * generating a string representation of the account.
+ * </p>
+ */
+
 public class LinkModel extends BaseModel {
 
+    /**
+     * This variable stores the format template for generating a descriptive string representation
+     * of a LinkModel instance. The format includes placeholders for the ID, Name, and CategoryId.
+     * The placeholders are:
+     * - %d for the ID (integer)
+     * - %s for the Name (string)
+     * - %d for the CategoryId (integer)
+     */
+    protected final String toStringFormat = "Link: Id=%d, Name=%s, CategoryId=%d";
+    /**
+     * Represents the category ID associated with a link model.
+     * This field is immutable and accessible only within the same package.
+     */
     private final int categoryId;
 
+    /**
+     * Constructs a new LinkModel with the specified ID, name, and category ID.
+     *
+     * @param id         the unique identifier for the LinkModel
+     * @param name       the name associated with the LinkModel. Name is used to store the lookup string
+     * @param categoryId the category ID associated with the LinkModel
+     */
     public LinkModel(int id, String name, int categoryId) {
         super(id, name);
         this.categoryId = categoryId;
     }
 
+    /**
+     * Retrieves the category ID associated with the LinkModel instance.
+     *
+     * @return the category ID of the LinkModel
+     */
     public int getCategoryId() {
         return this.categoryId;
     }
 
+    /**
+     * Returns a string representation of the LinkModel instance.
+     * The format includes placeholders for the ID, Name, and CategoryId.
+     *
+     * @return a formatted string representing the LinkModel instance
+     */
     @Override
     public String toString() {
-        return "Link: " +
-                "Id = " + this.id +
-                ", Name = " + this.name +
-                ", CategoryId = " + this.categoryId;
+        return String.format(toStringFormat, getID(), getName(), getCategoryId());
     }
 }
