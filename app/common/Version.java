1364d25fcf0b983eb316a1a3e5be0b2c638e358b
BB <biring@gmail.com>
2024-12-21 22:44:37 -0500

    chore(tests): increment build number after successful unit tests

--- app/common/Version.java
+++ app/common/Version.java
@@ -21,7 +21,7 @@ public class Version {
     private static final int patch = 0;
 
     // Build version number
-    private static final int build = 0;
+    private static final int build = 1;
 
     // Format string for version
     private static final String versionFormat = "Version %d.%d.%d";
