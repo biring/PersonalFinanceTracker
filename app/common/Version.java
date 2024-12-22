3d06183d170c608c8864e28821857ae9fbe75eba
BB <biring@gmail.com>
2024-12-22 14:16:51 -0500

    chore(script): add git pre commit hook script
    this is a tested script that should be placed in .git/hook folder

--- app/common/Version.java
+++ app/common/Version.java
@@ -21,7 +21,7 @@ public class Version {
     private static final int patch = 0;
 
     // Build version number
-    private static final int build = 43;
+    private static final int build = 44;
 
     // Format string for version
     private static final String versionFormat = "Version %d.%d.%d";
